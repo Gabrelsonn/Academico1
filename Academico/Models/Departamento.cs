e038ebfcacb1157bb3c9f9ef9b0bf55ae076393e
Gabrelsonn <gabrielseixas117@gmail.com>
2025-11-11 20:22:09 -0300

    Toma

---
+++ Academico/Models/Departamento.cs
@@ -0,0 +1,13 @@
+﻿namespace Academico.Models
+{
+    public class Departamento
+    {
+        public int DepartamentoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Email { get; set;}
+    }
+}
